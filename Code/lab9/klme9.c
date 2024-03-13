40a78821fafa487bafc6868c6da699913a20bf1d
Exate <144182792+Exaters@users.noreply.github.com>
2024-03-13 09:00:39 +0300

    11111

--- Code/lab9/klme9.c
+++ Code/lab9/klme9.c
@@ -10,6 +10,11 @@ int* videlenie(int);
 void fillrand(int*, int);
 void output(int*, int);
 void sravnenie(int*, int);
+/////////////////////////// даьше бога нет
+int pulse(int a, int b);
+int minusi(int a, int b);
+int mnogopluse(int a, int b);
+int mnogominusi(int a, int b);
 
 int main() {
     int (*funcpointer)();
@@ -36,6 +41,28 @@ int main() {
     arrimp(arr, size);
 
     free(arr);
+
+    int (*operations[4])(int, int) = {pulse, minusi, mnogopluse, mnogominusi};
+
+    int choice, result, num1, num2;
+
+    printf("\nInsert nums : ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("choose oper:\n");
+    printf("1. +\n");
+    printf("2. -\n");
+    printf("3. *\n");
+    printf("4. /\n");
+    printf("Your choice: ");
+    scanf("%d", &choice);
+
+    if (choice < 1 || choice > 4) {
+        printf("err incorrect oper\n");
+        return 1;
+    }
+
+    printf("Result: %d\n", operations[choice - 1](num1, num2));
     return 0;
 }
 
@@ -168,4 +195,25 @@ int oddsringevennumber() {
 
     printf("\n%d sum", sum);
     printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+}
+
+int pulse(int a, int b) {
+    return a + b;
+}
+
+int minusi(int a, int b) {
+    return a - b;
+}
+
+int mnogopluse(int a, int b) {
+    return a * b;
+}
+
+int mnogominusi(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        printf("Ошибка: деление на ноль\n");
+        return 0;
+    }
 }
