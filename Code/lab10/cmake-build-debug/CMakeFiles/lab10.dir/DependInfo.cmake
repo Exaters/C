4ef74179882cb76c9b9c2c3c868529863a598f76
Exate <144182792+Exaters@users.noreply.github.com>
2024-05-15 23:13:16 +0300

    HZ

--- Code/lab10/cmake-build-debug/CMakeFiles/lab10.dir/DependInfo.cmake
+++ Code/lab10/cmake-build-debug/CMakeFiles/lab10.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/short/OneDrive/Документы/GitHub/C/Code/lab10/klme10.c" "CMakeFiles/lab10.dir/klme10.c.obj" "gcc" "CMakeFiles/lab10.dir/klme10.c.obj.d"
+  "C:/Users/FSB/Documents/GitHub/C/Code/lab10/klme10.c" "CMakeFiles/lab10.dir/klme10.c.obj" "gcc" "CMakeFiles/lab10.dir/klme10.c.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
